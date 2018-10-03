cae4de8329b8bf899e6669278f58d6f927a57611
Hari Chaitanya Kodapaka <hkodapaka1211@conestogac.on.ca>
2018-10-02 22:35:28 -0400

    Inital commit - triangle solver file added

---
+++ Assignment2_QA/TriangleSolver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2_QA
+{
+    public static class TriangleSolver
+    {
+        public static string Analyze(int sideA, int sideB, int sideC)
+        {
+            if (ValidateSide(sideA) && ValidateSide(sideB) && ValidateSide(sideC))
+            {
+                if(ValidateTraingle(sideA, sideB, sideC))
+                {
+                    if ((sideA == sideB)&& (sideB == sideC))
+                    {
+                        return "Euqilataral Triangle Formed...";
+                    }else if ((sideA != sideB) &&(sideB != sideC) && (sideC!=sideA))
+                    {
+                        return "Scalene Triangle Formed...";
+                    }
+                    else
+                    {
+                        return "Isosceles Triangle Formed...";
+                    }
+                }
+                else
+                {
+                    return "Can not form ttriangle with given sides";
+                }
+            }
+            return "Invalid side entered..";
+        }
+
+        private static bool ValidateTraingle(int A, int B, int C)
+        {
+            if (((A + B) > C) && ((C + B) > A) && ((A + C) > B)) 
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private static bool ValidateSide(int side)
+        {
+            if(side <= 0)
+            {
+                Console.WriteLine("Input {0} is not a valid side!!", side);
+                return false;
+            }
+            return true;
+        }
+    }
+}
